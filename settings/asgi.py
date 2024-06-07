81e9d6e9b54ecd054f3d5177b97aa4463eea4113
Serhii <serhii.kolodyazhniy@gmail.com>
2024-06-07 22:29:16 +0300

    fix

--- settings/asgi.py
+++ settings/asgi.py
@@ -4,12 +4,13 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 from channels.auth import AuthMiddlewareStack
-from .consumers import ChatConsumer, BattleConsumer, RoomDeleteConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+from .consumers import ChatConsumer, BattleConsumer, RoomDeleteConsumer
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
